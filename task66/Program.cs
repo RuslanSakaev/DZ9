b6d61fe021bd8649e50d337c1d70bc514a28d647
RuslanSakaev <hardist@mail.ru>
2022-11-16 00:24:09 +0300

    Добавил решение задания 68

--- task66/Program.cs
+++ task66/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: 
+// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.Write($"M = {numM}, N = {numN} -> ");
+
+int Sum = sumNaturalElements(numM, numN);
+Console.Write(Sum);
+
+int sumNaturalElements (int numb1, int numb2)
+{
+    if (numb2 == numb1) return 0;
+    return sumNaturalElements(numb1, numb2 -1) * (numb2 - numb1 + 1) / 2;
+}
+
+// через тернарный оператор
+// int DegreeNumber (int numb1, int numb2)
+// {
+//     return numb2 == 0 ? 1 : numb1 * DegreeNumber (numb1, numb2-1);
+// }
--- task68/Program.cs
+++ task68/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: 
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите значение m:  ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.Write($"m = {numM}, n = {numN} -> A(m,n) = ");
+
+int Ack = ackerman(numM, numN);
+Console.Write(Ack);
+
+int ackerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return ackerman(m - 1, 1);
+    }
+    else
+    {
+        return ackerman(m - 1, ackerman(m, n - 1));
+    }
+}
+
